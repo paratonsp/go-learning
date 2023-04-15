4cf0f249971b2ff148113e9cdbb9c72f438da121
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-04-16 06:05:20 +0700

    cloneAuthRepo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# learning-go
- 
+# go-jwt-mux
+Tutorial mengamankan REST API menggunakan JWT, Mux middleware
+
+Youtube: [Golang JWT - Mengamankan REST API dengan JWT, Mux Middleware](https://youtu.be/YIaO5ZtHGF4)
--- main.go
+++ main.go
@@ -1,73 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"learning-go/middlewares"
+
+	"learning-go/controllers/authcontroller"
+	"learning-go/controllers/productcontroller"
+	"learning-go/models"
+
+	"github.com/gorilla/mux"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.JSON(http.StatusOK, albums)
-}
-
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, a := range albums {
-		if a.ID == id {
-			c.JSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.JSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	albums = append(albums, newAlbum)
-	c.JSON(http.StatusOK, newAlbum)
-}
-
-func deleteAlbum(c *gin.Context) {
-	id := c.Param("id")
-
-	for i, a := range albums {
-		if a.ID == id {
-			albums = append(albums[:i], albums[i+1:]...)
-			c.JSON(http.StatusOK, gin.H{"message": "Sukses"})
-			return
-		}
-	}
-
-	c.JSON(http.StatusInternalServerError, gin.H{"message": "Gagal"})
-
-}
-
 func main() {
-	router := gin.Default()
 
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
-	router.DELETE("/albums/:id", deleteAlbum)
+	models.ConnectDatabase()
+	r := mux.NewRouter()
 
-	router.Run("localhost:4000")
+	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
+	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
+	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/products", productcontroller.Index).Methods("GET")
+	api.Use(middlewares.JWTMiddleware)
+
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
