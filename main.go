e8f4ebb1ce3d6c506f2d351450b15bcc17dcf4af
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-04-17 05:11:31 +0700

    testDocker

--- main.go
+++ main.go
@@ -5,35 +5,35 @@ import (
 	"log"
 	"net/http"
 
-	"learning-go/middlewares"
+	// "learning-go/middlewares"
 
-	"learning-go/controllers/authcontroller"
-	"learning-go/controllers/productcontroller"
-	"learning-go/models"
+	// "learning-go/controllers/authcontroller"
+	// "learning-go/controllers/productcontroller"
+	// "learning-go/models"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	models.ConnectDatabase()
+	// models.ConnectDatabase()
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>learning-go</h1>")
 	})
 
-	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
-	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
-	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+	// r.HandleFunc("/login", authcontroller.Login).Methods("POST")
+	// r.HandleFunc("/register", authcontroller.Register).Methods("POST")
+	// r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
 
-	api := r.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/products", productcontroller.Get).Methods("GET")
-	api.HandleFunc("/products/{id}", productcontroller.GetById).Methods("GET")
-	api.HandleFunc("/products", productcontroller.Post).Methods("POST")
-	api.HandleFunc("/products/{id}", productcontroller.Update).Methods("PATCH")
-	api.HandleFunc("/products/{id}", productcontroller.Delete).Methods("DELETE")
-	api.Use(middlewares.JWTMiddleware)
+	// api := r.PathPrefix("/api").Subrouter()
+	// api.HandleFunc("/products", productcontroller.Get).Methods("GET")
+	// api.HandleFunc("/products/{id}", productcontroller.GetById).Methods("GET")
+	// api.HandleFunc("/products", productcontroller.Post).Methods("POST")
+	// api.HandleFunc("/products/{id}", productcontroller.Update).Methods("PATCH")
+	// api.HandleFunc("/products/{id}", productcontroller.Delete).Methods("DELETE")
+	// api.Use(middlewares.JWTMiddleware)
 
 	log.Fatal(http.ListenAndServe(":80", r))
 }
